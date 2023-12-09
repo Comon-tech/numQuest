fd6514e14f72f0914c29084ba4f8d203112c4dc9
C-o-m-o-n <comon928@gmail.com>
2023-12-10 00:47:14 +0300

    add score

--- index.js
+++ index.js
@@ -6,9 +6,15 @@ const guessInput =  document.getElementById('guessInput')
 const submitButton = document.querySelector('button');
 const messageBox = document.getElementById('message');
 const hintBox = document.getElementById('hint-message');
-
+const scoreBox = document.getElementById('score');
 
 let attempts = 0;
+// scoreBox.innerText = score;
+
+let scoreState = createState(100);
+
+// Display the initial score
+scoreBox.innerText = scoreState.getValue();
 
 function checkGuess() {
 // plyer's guess
@@ -26,15 +32,46 @@ const playerGuess = guessInput.value;
 	} else 	if (playerGuess  < targetNumber){
 		messageBox.id = 'message-error'
 		displayMessage(`Wrong guess. Try again.`);
-		   displayHint('Hint~> Try a higher number.');
+		displayHint('Hint~> Try a higher number.');
+
+		  // Update the score with a callback to immediately reflect the change
+  scoreState.setValue(scoreState.getValue() - 10, updatedScore => {
+    // Display the updated score
+    scoreBox.innerText = updatedScore;
+  });
+  
     } else if (playerGuess > targetNumber){
 		messageBox.id = 'message-error'
 		displayMessage(`Wrong guess. Try again.`);
         displayHint('hint~> Try a lower number.');
+
+		  // Update the score with a callback to immediately reflect the change
+  scoreState.setValue(scoreState.getValue() - 10, updatedScore => {
+    // Display the updated score
+    scoreBox.innerText = updatedScore;
+  });
+  
 	}else{
 		guessInput.value = ''
 	};
+
 };
+
+
+function resetGame() {
+	// Reset the game by setting all values back to their defaults
+	attempts = 0;
+	score = 100;
+	// targetNumber = Math.floor(Math.random() * 10) + 1;
+	guessInput.value = '';
+	guessInput.disabled = false;
+	submitButton.disabled = false;
+	messageBox.innerText = '';
+	hintBox.innerText = '';
+	scoreBox.innerText = '';
+	// alert('Game reset. Try again!');
+}
+
 
 function displayMessage	(message) {
 	messageBox.innerText = message;
@@ -50,3 +87,21 @@ function displayHint(hintMessage){
 	hintBox.innerText = hintMessage;
 };
 
+// Function to create a state object
+function createState(initialValue) {
+	let value = initialValue;
+  
+	function getValue() {
+	  return value;
+	}
+  
+	function setValue(newValue, callback) {
+	  value = newValue;
+	  if (typeof callback === 'function') {
+		callback(value);
+	  }
+	  return value;
+	}
+  
+	return { getValue, setValue };
+  }
